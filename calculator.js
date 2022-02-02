aef3afd01ff3047d670452fd1066780982deb33f
jonathanwh <jonathanwh78@gmail.com>
2022-02-02 16:48:54 +0000

    equals now working

--- calculator.js
+++ calculator.js
@@ -1,47 +1,51 @@
 `use strict`
 
 import * as DOM from './domchallenge.js';
+//added an object to store the values of the calcs and now when the new button equals is pressed only then are they printed
+let calculation = {
+
+}
 
 //create element
 DOM.btnAdd.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnSub.onclick = () => sub(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnDiv.onclick = () => div(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnMult.onclick = () => mult(DOM.inputOne.value, DOM.inputTwo.value);
-//DOM.btnEql.onclick = () => 
+DOM.btnEql.onclick = () => disp(calculation.inputOne,calculation.inputTwo,calculation.type, calculation.result);
 DOM.btnClear.onclick = () => clear();
 //Add
 function add (num1, num2)
 {
-    let op = "+";
-    let ans = 0;
-    ans = +num1 + +num2;
-    disp(num1, num2, op, ans);
+  calculation.inputOne = num1;
+  calculation.inputTwo = num2;
+  calculation.result=Number(num1)+Number(num2);
+  calculation.type = '+';
 }
 
 //subtract
 function sub (num1, num2)
 {
-    let op = "-";
-    let ans = 0;
-    ans = +num1 - +num2;
-    disp(num1, num2, op, ans);
+  calculation.inputOne = num1;
+  calculation.inputTwo = num2;
+  calculation.result=Number(num1)-Number(num2);
+  calculation.type = '-';
 }
 
 //divide
 function div (num1, num2)
 {
-    let op = "/";
-    let ans = 0;
-    ans = +num1 / +num2;
-    disp(num1, num2, op, ans);
+  calculation.inputOne = num1;
+  calculation.inputTwo = num2;
+  calculation.result=Number(num1)/Number(num2);
+  calculation.type = '/';
 }
 //multiply
 function mult (num1, num2)
 {
-    let op = "*";
-    let ans = 0;
-    ans = +num1 * +num2;
-    disp(num1, num2, op, ans);
+  calculation.inputOne = num1;
+  calculation.inputTwo = num2;
+  calculation.result= +num1 * +num2;
+  calculation.type = '*';
 }
 
 //Adds to list
