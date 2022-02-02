520ab85dec1a9f6960f59daff4195613f9a78a57
jonathanwh <jonathanwh78@gmail.com>
2022-02-02 15:52:15 +0000

    added clear button and full equation in output

--- domchallenge.js
+++ domchallenge.js
@@ -4,5 +4,7 @@ export const btnAdd = document.getElementById(`button_add`);
 export const btnSub = document.getElementById(`button_subtract`);
 export const btnMult = document.getElementById(`button_multiply`);
 export const btnDiv = document.getElementById(`button_divide`);
+export const btnClear = document.getElementById(`button_clear`);
 export const output = document.getElementById(`list_output`);
+
 
