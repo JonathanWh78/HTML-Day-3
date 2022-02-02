04b46c86d71c15732ebc16e94fe1662eba294584
jonathanwh <jonathanwh78@gmail.com>
2022-02-02 16:16:01 +0000

    working no equals tho

--- calculator.js
+++ calculator.js
@@ -7,6 +7,7 @@ DOM.btnAdd.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnSub.onclick = () => sub(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnDiv.onclick = () => div(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.btnMult.onclick = () => mult(DOM.inputOne.value, DOM.inputTwo.value);
+//DOM.btnEql.onclick = () => 
 DOM.btnClear.onclick = () => clear();
 //Add
 function add (num1, num2)
@@ -49,6 +50,7 @@ function disp(num1, num2, op, ans)
     let tst = document.createElement(`li`);
     tst.innerHTML = num1 + "" + op + "" + num2 + "=" + ans;
     DOM.output.appendChild(tst);
+    
 }
 
 //clear
--- domchallenge.js
+++ domchallenge.js
@@ -4,6 +4,7 @@ export const btnAdd = document.getElementById(`button_add`);
 export const btnSub = document.getElementById(`button_subtract`);
 export const btnMult = document.getElementById(`button_multiply`);
 export const btnDiv = document.getElementById(`button_divide`);
+export const btnEql = document.getElementById(`button_equal`);
 export const btnClear = document.getElementById(`button_clear`);
 export const output = document.getElementById(`list_output`);
 
