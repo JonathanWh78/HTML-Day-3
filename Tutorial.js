499bbd0d6719f7ff0476853c9cae0917477ce0c0
jonathanwh <jonathanwh78@gmail.com>
2022-02-02 09:34:12 +0000

    Task6

---
+++ Tutorial.js
@@ -0,0 +1,9 @@
+let car = new Object();
+car["make"] = "Audi";
+car["model"] = "A5";
+car["carReg"] = "W8M8";
+
+console.log(car) // shows all attributes
+console.log(car.make); //show the value of make
+console.log(car.model); //show the value of model
+console.log(car.carReg); //show the value of carReg
